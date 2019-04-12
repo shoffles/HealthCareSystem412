74b4eacd7de42cda311a3ef28f8e7f017a17f9c2
KonnorSidler <36972046+KonnorSidler@users.noreply.github.com>
2019-04-12 14:47:59 -0400

    Working UserAuth Class
    Loading userlist and creating a user both inplemented with postgres database

--- src/healthcaresystem412/UserAuth.java
+++ src/healthcaresystem412/UserAuth.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 
 
@@ -21,34 +22,13 @@ public class UserAuth {
     public static HashMap<String, User> map = new HashMap<>();
     
     public UserAuth() {
-        String SQL = "SELECT user_name,user_type,user_password,user_first_last,user_dob " + 
-                "FROM appuser ";
-        
-        try (Connection sqlConnection = PostgresConnector.connect();
-                PreparedStatement prepState = sqlConnection.prepareStatement(SQL)) {
-            
-            ResultSet results = prepState.executeQuery();
-            
-            while (results.next()) {
-                User userToAdd = new User(results.getString("user_name"),
-                        results.getString("user_type"),
-                        results.getString("user_password"),
-                        results.getString("user_first_last"),
-                        results.getString("user_dob"));
-                
-                        
-                this.map.put(userToAdd.getUsername(), userToAdd);
-            }
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        loadUserMap();
     }
 
     
     public void loadUserMap() {
         String SQL = "SELECT user_name,user_type,user_password,user_first_last,user_dob " + 
-                "FROM user ";
+                "FROM appuser ";
         
         try (Connection sqlConnection = PostgresConnector.connect();
                 PreparedStatement prepState = sqlConnection.prepareStatement(SQL)) {
@@ -71,12 +51,50 @@ public class UserAuth {
     }
     
     public boolean addMapping(String username, String password, String type, String name, String DOB){
+        
+        boolean userValid = false;
+        
         if(this.map.containsKey(username)) {
-            return false;
+            userValid = false;
+        } else {
+            userValid = true;
+            
+            User newUser = new User(username, type.toLowerCase(), password, name, DOB);
+            this.map.put(newUser.getUsername(), newUser);
+            /*
+
+
+            */
+            String SQL = "INSERT INTO appuser(user_name,user_type,user_password,user_first_last,user_dob) " 
+                    + "VALUES(?,?,?,?,?)";
+
+            long id = 0;
+
+
+
+            try (Connection sqlConnection = PostgresConnector.connect();
+                    PreparedStatement prepState = sqlConnection.prepareStatement(SQL)) {
+
+                prepState.setString(1, username);
+                prepState.setString(2, type.toLowerCase());
+                prepState.setString(3, password);
+                prepState.setString(4, name);
+                prepState.setString(5, DOB);
+
+
+                int rowCount = prepState.executeUpdate();
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            /*
+
+
+            */
+            userValid = true;
         }
-        User user = new User(username, type.toLowerCase(), password, name, DOB);
-        this.map.put(user.getUsername(), user);
-        return true;
+        
+        return userValid;
     }
     
     public boolean checkUsernamePassword(String username, String password) {
