6a1aa20af905284ffeb5391897414d20b10b2eab
StevenWeberPSU <sfw5183@gmail.com>
2019-04-20 04:17:02 -0400

    Export Report functionality

--- src/healthcaresystem412/FXMLDoctorDashboardController.java
+++ src/healthcaresystem412/FXMLDoctorDashboardController.java
@@ -5,6 +5,10 @@
  */
 package healthcaresystem412;
 
+import java.awt.Dimension;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
@@ -13,6 +17,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -27,7 +33,13 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBuilder;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.swing.JFileChooser;
 
 /**
  * FXML Controller class
@@ -47,22 +59,21 @@ public class FXMLDoctorDashboardController extends Controller implements Initial
     private TableView<Report> table;
 
 //    public ObservableList<Report> list = FXCollections.observableArrayList(ReportList.reports);
-            
     @FXML
     private TableColumn<Report, String> patientNameCol;
     @FXML
     private TableColumn<Report, String> titleCol;
-            
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // ArrayList to store UserId's of who the main user may view
         ArrayList<Integer> userIDs = new ArrayList<>();
 
         // SQL Statment string to display who's reports the user may view
-        String SQL = "SELECT appuser.user_id,doctor_to_patient_assignment.to_be_viewed_id " + 
-                "FROM appuser " + 
-                "INNER JOIN doctor_to_patient_assignment ON appuser.user_id=doctor_to_patient_assignment.viewer_id " + 
-                "WHERE user_id = ?";
+        String SQL = "SELECT appuser.user_id,doctor_to_patient_assignment.to_be_viewed_id "
+                + "FROM appuser "
+                + "INNER JOIN doctor_to_patient_assignment ON appuser.user_id=doctor_to_patient_assignment.viewer_id "
+                + "WHERE user_id = ?";
 
         // Connection to the database to run query
         try (Connection sqlConnection = PostgresConnector.connect();
@@ -80,37 +91,37 @@ public class FXMLDoctorDashboardController extends Controller implements Initial
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        
+
         // Declares an ArrayList of reports that were loaded from the ReportList class
         ArrayList<Report> data = ReportList.loadReportList(userIDs);
-            
+
         // Converts the data ArrayList into something JavaFX can use for table setting
         ObservableList<Report> list = FXCollections.observableArrayList(data);
-    
+
         // Sets the values of the tables to be viewed
-        patientNameCol.setCellValueFactory(new PropertyValueFactory<Report,String>("name"));
-        titleCol.setCellValueFactory(new PropertyValueFactory<Report,String>("reportTitle"));
-        bodyCol.setCellValueFactory(new PropertyValueFactory<Report,String>("reportBody"));
+        patientNameCol.setCellValueFactory(new PropertyValueFactory<Report, String>("name"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<Report, String>("reportTitle"));
+        bodyCol.setCellValueFactory(new PropertyValueFactory<Report, String>("reportBody"));
         table.setItems(list);
-    }    
+    }
 
     @FXML
     private void handleBackButtonAction(ActionEvent event) throws IOException {
         System.out.println("Logged out");
         Parent loginParent = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
         Scene loginScene = new Scene(loginParent);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(loginScene);
         window.show();
-    
+
     }
-    
+
     @FXML
-    private void handleEditPatientsAction(ActionEvent event) throws IOException {
+    private void handleCreateAssignmentAction(ActionEvent event) throws IOException {
         System.out.println("Moving to Edit Patients");
         Parent parent = FXMLLoader.load(getClass().getResource("FXMLEditPatients.fxml"));
         Scene editPatientScene = new Scene(parent);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(editPatientScene);
         window.show();
     }
@@ -120,11 +131,51 @@ public class FXMLDoctorDashboardController extends Controller implements Initial
         System.out.println("Loading report creation");
         Parent reportParent = FXMLLoader.load(getClass().getResource("FXMLReportCreation.fxml"));
         Scene reportCreationScene = new Scene(reportParent);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(reportCreationScene);
         window.show();
     }
-    
-  
-    
+
+    @FXML
+    private void handleExportAction(ActionEvent event) throws IOException {
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setTitle("Report Export");
+        
+        Report report = table.getSelectionModel().getSelectedItem();
+        String selected = "";
+        selected = "Report Title: " + report.getReportTitle()
+                + "\nPatient: " + report.getUsername()
+                + "\nReport Body: " + report.getReportBody();
+        System.out.println(selected);
+
+       
+
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(window);
+
+        if (file != null) {
+            SaveFile(selected, file);
+        }
+
+        window.show();
+
+    }
+
+    private void SaveFile(String content, File file) {
+        try {
+            FileWriter fileWriter = null;
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            //   Logger.getLogger(JavaFX_Text.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
--- src/healthcaresystem412/FXMLPatientDashboardController.java
+++ src/healthcaresystem412/FXMLPatientDashboardController.java
@@ -5,6 +5,8 @@
  */
 package healthcaresystem412;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
@@ -28,6 +30,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -45,10 +48,9 @@ public class FXMLPatientDashboardController extends Controller implements Initia
     private TableColumn<Report, String> titleCol;
     @FXML
     private TableColumn<Report, String> bodyCol;
-    
+
     @FXML
     private TableView<Report> table;
-
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -56,10 +58,10 @@ public class FXMLPatientDashboardController extends Controller implements Initia
         ArrayList<Integer> userIDs = new ArrayList<>();
 
         // SQL Statment string to display who's reports the user may view
-        String SQL = "SELECT appuser.user_id,doctor_to_patient_assignment.to_be_viewed_id " + 
-                "FROM appuser " + 
-                "INNER JOIN doctor_to_patient_assignment ON appuser.user_id=doctor_to_patient_assignment.viewer_id " + 
-                "WHERE user_id = ?";
+        String SQL = "SELECT appuser.user_id,doctor_to_patient_assignment.to_be_viewed_id "
+                + "FROM appuser "
+                + "INNER JOIN doctor_to_patient_assignment ON appuser.user_id=doctor_to_patient_assignment.viewer_id "
+                + "WHERE user_id = ?";
 
         // Connection to the database to run query
         try (Connection sqlConnection = PostgresConnector.connect();
@@ -77,32 +79,72 @@ public class FXMLPatientDashboardController extends Controller implements Initia
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        
+
         // Declares an ArrayList of reports that were loaded from the ReportList class
         ArrayList<Report> data = ReportList.loadReportList(userIDs);
-        
+
         // Converts data to an ArrayList that JavaFX can work with for setting tables
         ObservableList<Report> list = FXCollections.observableArrayList(data);
-        
+
         // Sets table values
-        idCol.setCellValueFactory(new PropertyValueFactory<Report,String>("reportId"));
-        bodyCol.setCellValueFactory(new PropertyValueFactory<Report,String>("reportBody"));
-        titleCol.setCellValueFactory(new PropertyValueFactory<Report,String>("reportTitle"));
+        idCol.setCellValueFactory(new PropertyValueFactory<Report, String>("reportId"));
+        bodyCol.setCellValueFactory(new PropertyValueFactory<Report, String>("reportBody"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<Report, String>("reportTitle"));
         table.setItems(list);
-    }    
+    }
 
     @FXML
     private void handleBackButtonAction(ActionEvent event) throws IOException {
         System.out.println("Logged out");
         Parent loginParent = FXMLLoader.load(getClass().getResource("FXMLLogin.fxml"));
-        
+
         Scene loginScene = new Scene(loginParent);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(loginScene);
         window.show();
-    
+
     }
-    
-    
-    
+
+ @FXML
+    private void handleExportAction(ActionEvent event) throws IOException {
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setTitle("Report Export");
+        
+        Report report = table.getSelectionModel().getSelectedItem();
+        String selected = "";
+        selected = "Report Title: " + report.getReportTitle()
+                + "\nPatient: " + report.getUsername()
+                + "\nReport Body: " + report.getReportBody();
+        System.out.println(selected);
+
+       
+
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(window);
+
+        if (file != null) {
+            SaveFile(selected, file);
+        }
+
+        window.show();
+
+    }
+
+    private void SaveFile(String content, File file) {
+        try {
+            FileWriter fileWriter = null;
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            //   Logger.getLogger(JavaFX_Text.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
